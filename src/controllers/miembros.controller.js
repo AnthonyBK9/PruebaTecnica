6fa3001a1d47e346277108aefd9d78890adfdfea
AnthonyBK9 <antonio.bermudez.ing@gmail.com>
2023-01-21 21:21:25 -0600

    adding validation of the received data

--- src/controllers/miembros.controller.js
+++ src/controllers/miembros.controller.js
@@ -1,25 +1,54 @@
 import { pool } from "../utils/database.js";
 
-export const getMiembros = async (req, res) => {
+export const getMiembrosByIdorName = async (req, res) => {
     const { id, nombre } = req.body;
-    const pkmember = parseInt(id);
-    const [ rows ] = await pool.query('call sp_getmiembroByIdOrName(?,?)',[pkmember, nombre]);
-    res.status(200).json(
-        {
-            msg: 'Get sucess', 
-            data: rows[0]
-        });
+
+    if ( id, nombre) {
+
+        const pkmember = parseInt(id);
+        const [ rows ] = await pool.query('call sp_getmiembroByIdOrName(?,?)',[pkmember, nombre]);
+        res.status(200).json(
+            {
+                msg: 'Get success', 
+                data: rows[0]
+            });
+
+    } else {
+
+        res.status(400).json(
+            {
+                mgs: 'Los campos son obligatorios',
+                fields: {
+                    id: 'string',
+                    nombre: 'string',
+                }
+            }
+        );
+    }
 }
 
 export const createMiembros = async (req, res) => {
     const { nombre, direccion, codigo } = req.body;
-    const [ rows ] = await pool.query(`INSERT INTO miembros (nombre, direccion, codigo ) VALUES (?, ?, ?)`,[nombre, direccion, codigo])
-    res.status(201).json(
-        {
-            msg: 'Post sucess', 
-            id: rows.insertId, 
-            nombre, 
-            direccion, 
-            codigo
-        });
+    if( NavigationPreloadManager, direccion, codigo ) {
+        const [ rows ] = await pool.query(`INSERT INTO miembros (nombre, direccion, codigo ) VALUES (?, ?, ?)`,[nombre, direccion, codigo])
+        res.status(201).json(
+            {
+                msg: 'Post success', 
+                id: rows.insertId, 
+                nombre, 
+                direccion, 
+                codigo
+            });
+    } else {
+        res.status(400).json(
+            {
+                msg: 'Todos los campos son obligatorios',
+                fields: {
+                    nombre: 'String',
+                    direccion: 'String',
+                    codigo: 'String'
+                }
+            }
+        );
+    }
 }
