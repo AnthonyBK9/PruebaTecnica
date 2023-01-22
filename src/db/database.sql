3eb938d018bc56155d8200146e3f5b268de3113a
AnthonyBK9 <antonio.bermudez.ing@gmail.com>
2023-01-21 21:23:02 -0600

    adding trim to sql query

--- src/db/database.sql
+++ src/db/database.sql
@@ -12,26 +12,26 @@ CREATE TABLE miembros (
 
 INSERT INTO miembros ( nombre, direccion, codigo) 
     VALUES 
-        ( 'Juan Lípez', 'Calle x', '1575'),
-        ( 'Antonio Bermúdez', 'Calle 24', '94330'),
-        ( 'Viridiana Flores', 'Calle 10', '64544'),
-        ( 'Alex Martinez', 'Av 2', '94500'),
-        ( 'Mariela Cruz', 'Av 6', '94500'),
-        ( 'Vannesa', 'Av 10', '94500'),
-        ( 'Ivone', 'Av 10', '94500'),
-        ( 'Moises Flores', 'Calle 42', '94500'),
-        ( 'Luis Alberto Pérez', 'Calle 2', '64544'),
-        ('Juan Gonzalez', 'Calle 10', '94330'),
-        ('Juan Alberto Muñoz', 'Av 4', '64544'),
-        ('Antonio Gutiérrez', 'Av 10', '94500'),
-        ('Alberto Zurita', 'Calle 12', '94600'),
-        ('Moises Pérez','Calle 24', '94600'),
-        ('Sergui Pérez','Calle 30', '94500');
+        ( 'Juan Lípez', 'Calle x', '1575' ),
+        ( 'Antonio Bermúdez', 'Calle 24', '94330' ),
+        ( 'Viridiana Flores', 'Calle 10', '64544' ),
+        ( 'Alex Martinez', 'Av 2', '94500' ),
+        ( 'Mariela Cruz', 'Av 6', '94500' ),
+        ( 'Vannesa', 'Av 10', '94500' ),
+        ( 'Ivone', 'Av 10', '94500' ),
+        ( 'Moises Flores', 'Calle 42', '94500' ),
+        ( 'Luis Alberto Pérez', 'Calle 2', '64544' ),
+        ( 'Juan Gonzalez', 'Calle 10', '94330' ),
+        ( 'Juan Alberto Muñoz', 'Av 4', '64544' ),
+        ( 'Antonio Gutiérrez', 'Av 10', '94500' ),
+        ( 'Alberto Zurita', 'Calle 12', '94600' ),
+        ( 'Moises Pérez','Calle 24', '94600' ),
+        ( 'Sergui Pérez','Calle 30', '94500' );
 
 delimiter $$
 	CREATE PROCEDURE sp_getmiembroByIdOrName(in _id int, in _nombre varchar(250))
     BEGIN
-		SELECT * FROM miembros WHERE Pkmember = _id OR nombre LIKE concat(_nombre,'%');
+		SELECT * FROM miembros WHERE Pkmember = _id OR nombre LIKE concat(trim(_nombre),'%');
     END;
 $$
 delimiter ;
